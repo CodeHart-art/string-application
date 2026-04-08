b45888de67a461329217d1b87a6ac5d6e229e96d
CodeHart-art <jobhart11211@gmail.com>
2026-04-08 15:06:08 -0400

    Step 1 and 2

---
+++ src/main/java/VariableApp.java
@@ -0,0 +1,24 @@
+public class VariableApp {
+    public static void main(String[] args) {
+        // Step 1
+        String faveColor;
+        int yearStarted;
+        char middleInitial;
+        boolean hasPet;
+        String niceMessage;
+
+        // Step 2
+        int numOfDayInWeek;
+        numOfDayInWeek = 7;
+
+        double coffeePrice;
+        coffeePrice = 4.99;
+
+        char faveLetter;
+        faveLetter = 'H';
+
+        boolean isRaining;
+        isRaining = true;
+
+    }
+}
